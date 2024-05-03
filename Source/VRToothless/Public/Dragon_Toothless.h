1fcb876a27f5cebe40f167f0a12cbcbff0e2ecca
seoyoung7879 <hg2263@naver.com>
2024-05-03 17:54:04 +0900

    qq

--- Source/VRToothless/Public/Dragon_Toothless.h
+++ Source/VRToothless/Public/Dragon_Toothless.h
@@ -30,8 +30,6 @@ public:
 		UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	class USkeletalMeshComponent* meshComp1;
 
-	UFUNCTION()
-	void OnOverlapPlayer(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	
 
 };
